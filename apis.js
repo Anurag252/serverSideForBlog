e74e6762726ed6191b8925dc1ed445417b994626
Anurag <Anuragsgsits@gmail.com>
2018-09-04 22:59:43 +0530

    [Addon] adding compiled Js file

---
+++ apis.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var json = assign();
+function assign() {
+    json = JSON.parse(" {\n            \"response\": [\n              \n                { \n                  \"question\": \"what is csharp\",\n                  \"answer\": \"this is csharp\"\n                }\n              \n            ]\n          }");
+    return json;
+}
+function addData(data) {
+    if (data !== undefined) {
+        json.response.push(new Responses(data.question, data.answer));
+        console.log(json);
+        return null;
+    }
+    else {
+        return json;
+    }
+}
+var Responses = /** @class */ (function () {
+    function Responses(question, answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+    return Responses;
+}());
+var server = http.createServer(function (req, res) {
+    console.log(json);
+    var jsonData = JSON.stringify(json);
+    if (req.url = "/chsarp") {
+        if (req.method == "GET")
+            res.write(jsonData);
+        if (req.method == "POST") {
+            req.on('data', function (data) {
+                var resp = JSON.parse(data);
+                addData(resp);
+            });
+            req.on('end', function () {
+            });
+        }
+    }
+    if (req.url = "/angular") {
+        res.write("");
+    }
+    if (req.url = "/webApi") {
+        res.write("");
+    }
+    if (req.url = "/sapsecurity") {
+        res.write("");
+    }
+    res.end();
+});
+server.on('clientError', function (err, socket) {
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+server.listen(8000);
